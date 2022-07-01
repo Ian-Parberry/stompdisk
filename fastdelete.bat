f6885560d5c903820de7057a6ea2f8950a4f0213
Ian Parberry <Ian-Parberry@users.noreply.github.com>
2022-07-01 17:26:39 -0500

    Update fastdelete.bat

--- fastdelete.bat
+++ fastdelete.bat
@@ -40,7 +40,7 @@ if exist "%NAME%" (
   set /p CONFIRM="Are you sure you want to delete "%NAME%" [y/n]? "
   
   if "!CONFIRM!" == "y" (
-    echo "This may take some time..."
+    echo This may take some time...
     del /F/Q/S "%NAME%" > NUL
     rmdir /Q/S "%NAME%"
   ) else echo Delete aborted.
